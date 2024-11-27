474636e18ffa89e5099e88346bfab5f1f713344d
Ricardo Pascua Jr <rdpascua@outlook.com>
2024-11-27 19:10:06 +0800

    include html for lsp

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# 
+# Dotfiles
 
 brew install git-delta
+
+npm i -g vscode-langservers-extracted tsserver
--- nvim/init.lua
+++ nvim/init.lua
@@ -618,7 +618,7 @@ require('lazy').setup({
         },
         templ = {},
         tailwindcss = {},
-        tsserver = {},
+        ts_ls = {},
         -- tsserver = {
         --   on_attach = function(client)
         --     -- this is important, otherwise tsserver will format ts/js
@@ -872,7 +872,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'css' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
