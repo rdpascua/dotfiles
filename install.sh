a4f95d936b1fa8e96e79c8c5f8d0d77b25dfd805
Ricardo Pascua Jr <rdpascua@outlook.com>
2026-03-03 23:39:55 +0800

    fix installscript

--- install.sh
+++ install.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
-if ! command -v brew &> /dev/null
-then
-    echo "Homebrew not found. Installing Homebrew..."
+set -e
+
+DOTFILES="$HOME/.dotfiles"
+
+# Homebrew
+if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Homebrew is already installed."
 fi
 
+# Brew packages
+echo "Installing brew packages..."
+brew install neovim git-delta zoxide atuin
+
+# Oh My Zsh (UNATTENDED prevents it from hijacking the shell)
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "Oh My Zsh not found. Installing Oh My Zsh..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-    echo "Oh My Zsh is already installed."
+    echo "Installing Oh My Zsh..."
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-echo "Installing atuin"
-/bin/bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)"
+# Zsh plugins
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
 
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/nvim ~/.config/nvim
-ln -s ~/.dotfiles/ideavim/.ideavimrc ~/.ideavimrc
-ln -s ~/.dotfiles/atuin ~/.config/atuin
+# Create config directory
+mkdir -p "$HOME/.config"
+
+# Symlinks (force to overwrite existing)
+ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/git/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES/ideavim/.ideavimrc" "$HOME/.ideavimrc"
+ln -sfn "$DOTFILES/nvim" "$HOME/.config/nvim"
+ln -sfn "$DOTFILES/atuin" "$HOME/.config/atuin"
+
+echo "Done! Restart your shell and run :Lazy sync in Neovim."
