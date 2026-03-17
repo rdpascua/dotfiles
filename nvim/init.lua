e6714320610f89a71b8d2f3d9f66010fff794f31
Ricardo Pascua Jr <rdpascua@outlook.com>
2026-03-18 01:02:39 +0800

    add patched berkeley mono nerd font and fix treesitter config
    - Patched Berkeley Mono with Nerd Font icons for nvim icon support
    - Updated nvim-treesitter config for new API (removed deprecated configs module)

--- nvim/init.lua
+++ nvim/init.lua
@@ -904,29 +904,18 @@ require('lazy').setup({
         'json', 'kotlin', 'lua', 'markdown', 'markdown_inline', 'php',
         'templ', 'tsx', 'typescript', 'vim', 'vimdoc', 'yaml',
       },
-      -- Autoinstall languages that are not installed
       auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
     },
     config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      require('nvim-treesitter').setup(opts)
 
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      -- Enable treesitter-based highlighting and indentation
+      vim.treesitter.start = vim.treesitter.start or function() end
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
     end,
   },
 
